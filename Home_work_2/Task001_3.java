913c0cd1b5efd8145469fac215dec5e04b5cc642
Akuloff <akuloff.m@gmail.com>
2022-11-30 16:53:53 +0300

    Add Task001_3 project

---
+++ Home_work_2/Task001_3.java
@@ -0,0 +1,29 @@
+//package Home_work_2;
+
+/**
+ * Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float), и возвращает введенное значение. 
+ * Ввод текста вместо числа не должно приводить к падению приложения, вместо этого, 
+ * необходимо повторно запросить у пользователя ввод данных.
+ */
+
+import java.util.Scanner;
+
+public class Task001_3 {
+    public static void main(String[] args) {
+        checkFloat();
+    }
+
+    public static float checkFloat() {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Введите дробное число (разделяющий символ целой части - '.'): ");
+        float num;
+        while (!sc.hasNextFloat()) {
+            System.out.print("Ошибка! Вы ввели не дробное число, попробуйте ещё разок: ");
+            sc.next();
+        }
+        num = sc.nextFloat();
+        System.out.println("Вы ввели число: " + num);
+        sc.close();
+        return num;
+    }
+}
