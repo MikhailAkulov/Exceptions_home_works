9c36e0621169929119f1321c21cb768557ebf4c3
Akuloff <akuloff.m@gmail.com>
2022-11-30 17:01:50 +0300

    Update Task003 project

--- Home_work_2/Task003.java
+++ Home_work_2/Task003.java
@@ -32,6 +32,8 @@ public class Task003 {
             printSum(23, 234);
             int[] abc = { 1, 2 };
             abc[3] = 9;
+        } catch (ArithmeticException ex) {
+            System.out.println("Нельзя делить на ноль");
         } catch (NullPointerException ex) {
             System.out.println("Указатель не может указывать на null!");
         } catch (IndexOutOfBoundsException ex) {
